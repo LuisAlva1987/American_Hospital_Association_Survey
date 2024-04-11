77f1ff1b3d7b66f5db61da79961021f22096f92c
Luis102487 <96627296+Luis102487@users.noreply.github.com>
2024-04-11 19:57:53 -0400

    Update questions_queries.sql

--- questions_queries.sql
+++ questions_queries.sql
@@ -7,3 +7,13 @@ FROM
   luisalva.hopitals_patients_survey.responses
 WHERE
   release_period = '07_2023'; 
+
+
+--What was the year where most hospitals participated in the survey?
+
+
+--How hospitals participation has been performing? how volume of participation has increase or decrease over the years? Do a function window maybe.
+
+--What recommendations can you make to hospitals to help them further improve the patient experience?
+
+--Have hospitals' HCAHPS scores improved over the past 9 years?
